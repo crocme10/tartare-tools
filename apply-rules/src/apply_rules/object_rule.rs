48d6ad5406961eb24959444ed357aaad46d1d9a2
Jean SIMARD <woshilapin@tuziwo.info>
2020-06-16 15:38:19 +0200

    [tech] Refactorization of object_rule

--- apply-rules/src/apply_rules/object_rule.rs
+++ apply-rules/src/apply_rules/object_rule.rs
@@ -13,25 +13,25 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>
 
 use crate::apply_rules::ReportCategory;
-use failure::format_err;
+use failure::{bail, format_err};
 use log::info;
 use relational_types::IdxSet;
-use serde::Deserialize;
+use serde::{de::DeserializeOwned, Deserialize};
 use serde_json::Value;
 use std::{
-    collections::{HashMap, HashSet},
+    collections::{BTreeSet, HashMap},
     convert::TryFrom,
     fs::File,
+    ops::IndexMut,
     path::Path,
 };
 use tartare_tools::report::Report;
 use transit_model::{
     model::{Collections, Model},
-    objects::{
-        CommercialMode, Line, Network, ObjectType as ModelObjectType, PhysicalMode, VehicleJourney,
-    },
+    objects::{Line, ObjectType as ModelObjectType, TicketUsePerimeter, VehicleJourney},
     Result,
 };
+use typed_index_collection::{CollectionWithId, Id};
 
 #[derive(Debug, Deserialize)]
 pub struct ObjectProperties {
@@ -67,13 +67,46 @@ impl TryFrom<&Path> for ObjectRuleConfiguration {
 pub struct ObjectRule {
     configuration: ObjectRuleConfiguration,
     lines_by_network: Option<HashMap<String, IdxSet<Line>>>,
+    ticket_use_perimeters_by_network: Option<HashMap<String, IdxSet<TicketUsePerimeter>>>,
     lines_by_commercial_mode: Option<HashMap<String, IdxSet<Line>>>,
     vjs_by_physical_mode: Option<HashMap<String, IdxSet<VehicleJourney>>>,
 }
 
 impl ObjectRule {
+    fn check_configuration(configuration: &ObjectRuleConfiguration) -> Result<()> {
+        fn check_rules<'a>(rules: &'a [ObjectProperties], id_key: &str) -> Result<()> {
+            let mut used_identifiers = BTreeSet::new();
+            let mut insert_id = |s: &'a str| -> Result<()> {
+                if used_identifiers.insert(s) {
+                    Ok(())
+                } else {
+                    bail!("The {} \"{}\" is present multiple times in the configuration file which is invalid.", id_key, s)
+                }
+            };
+            for rule in rules {
+                let id = rule.check(id_key)?;
+                insert_id(id)?;
+                for regroup_id in &rule.grouped_from {
+                    insert_id(regroup_id.as_str())?;
+                }
+            }
+            Ok(())
+        }
+        if let Some(ref networks_rules) = configuration.networks_rules {
+            check_rules(&networks_rules, "network_id")?;
+        }
+        if let Some(ref commercial_modes_rules) = configuration.commercial_modes_rules {
+            check_rules(&commercial_modes_rules, "commercial_mode_id")?;
+        }
+        if let Some(ref physical_modes_rules) = configuration.physical_modes_rules {
+            check_rules(&physical_modes_rules, "physical_mode_id")?;
+        }
+        Ok(())
+    }
+
     pub(crate) fn new(path: &Path, model: &Model) -> Result<Self> {
         let configuration = ObjectRuleConfiguration::try_from(path)?;
+        ObjectRule::check_configuration(&configuration)?;
         let lines_by_network = if configuration.networks_rules.is_some() {
             Some(
                 model
@@ -88,6 +121,22 @@ impl ObjectRule {
                         }
                     })
                     .collect(),
+            )
+        } else {
+            None
+        };
+        let ticket_use_perimeters_by_network = if configuration.networks_rules.is_some() {
+            Some(
+                model
+                    .ticket_use_perimeters
+                    .iter()
+                    .filter(|(_, tup)| tup.object_type == ModelObjectType::Network)
+                    .fold(HashMap::new(), |mut map, (idx, tup)| {
+                        map.entry(tup.object_id.clone())
+                            .or_insert_with(IdxSet::new)
+                            .insert(idx);
+                        map
+                    }),
             )
         } else {
             None
@@ -131,6 +180,7 @@ impl ObjectRule {
         let object_rule = ObjectRule {
             configuration,
             lines_by_network,
+            ticket_use_perimeters_by_network,
             lines_by_commercial_mode,
             vjs_by_physical_mode,
         };
@@ -138,178 +188,85 @@ impl ObjectRule {
     }
 }
 
-fn check_and_apply_physical_modes_rules(
-    report: &mut Report<ReportCategory>,
-    collections: &mut Collections,
-    physical_modes_rules: &[ObjectProperties],
-    vjs_by_physical_mode: &HashMap<String, IdxSet<VehicleJourney>>,
-) -> Result<()> {
-    info!("Checking physical modes rules.");
-    let mut physical_modes_to_remove: HashSet<String> = HashSet::new();
-    let mut new_physical_modes: Vec<PhysicalMode> = vec![];
-
-    for pyr in physical_modes_rules.iter() {
-        let physical_mode_id = pyr
+impl ObjectProperties {
+    fn check(&self, id_key: &'_ str) -> Result<&str> {
+        let id = self
             .properties
-            .get("physical_mode_id")
-            .ok_or_else(|| format_err!("Key \"physical_mode_id\" is required"))?
+            .get(id_key)
+            .ok_or_else(|| format_err!("Key \"{}\" is required", id_key))?
             .as_str()
-            .ok_or_else(|| format_err!("Value for \"physical_mode_id\" must be filled in"))?;
+            .ok_or_else(|| format_err!("Value for \"{}\" must be filled in", id_key))?;
 
-        if !collections.physical_modes.contains_id(physical_mode_id) {
-            new_physical_modes.push(serde_json::from_value(pyr.properties.clone())?)
-        }
+        Ok(id)
+    }
 
-        let mut physical_mode_rule = false;
-        for pm_grouped in &pyr.grouped_from {
-            if !collections.physical_modes.contains_id(&pm_grouped) {
+    fn regroup<T, F>(
+        &self,
+        id: &'_ str,
+        collection: &CollectionWithId<T>,
+        report: &mut Report<ReportCategory>,
+        mut update: F,
+    ) -> Result<bool>
+    where
+        F: FnMut(&str, &str) -> bool,
+    {
+        let mut changed = false;
+        for grouped_id in &self.grouped_from {
+            if !collection.contains_id(&grouped_id) {
                 report.add_error(
-                    format!("The grouped physical mode \"{}\" don't exist", pm_grouped),
+                    format!("The identifier \"{}\" doesn't exist, and therefore cannot be regrouped in \"{}\"", grouped_id, id),
                     ReportCategory::ObjectNotFound,
                 );
-            } else if let Some(trips) = vjs_by_physical_mode.get(pm_grouped) {
-                for trip_idx in trips {
-                    collections
-                        .vehicle_journeys
-                        .index_mut(*trip_idx)
-                        .physical_mode_id = physical_mode_id.to_string();
-                }
-                physical_mode_rule = true;
-                physical_modes_to_remove.insert(pm_grouped.to_string());
+            } else {
+                changed = update(id, grouped_id) || changed;
             }
         }
-        if !physical_mode_rule {
+        Ok(changed)
+    }
+    fn apply<T, F>(
+        &self,
+        id_key: &'_ str,
+        collection: &mut CollectionWithId<T>,
+        report: &mut Report<ReportCategory>,
+        update: F,
+    ) -> Result<()>
+    where
+        T: DeserializeOwned + Id<T>,
+        F: FnMut(&str, &str) -> bool,
+    {
+        let id = self.check(id_key)?;
+        let created = if !collection.contains_id(id) {
+            collection.push(serde_json::from_value(self.properties.clone())?)?;
+            true
+        } else {
+            false
+        };
+
+        let rule_applied = self.regroup(id, collection, report, update)?;
+        if rule_applied {
+            collection.retain(|object| {
+                !self
+                    .grouped_from
+                    .iter()
+                    .any(|grouped_id| object.id() == grouped_id)
+            });
+        } else if created {
+            report.add_warning(
+                    format!(
+                        "Object with {} \"{}\" was created but must be used (through properties_rules) or else it will be deleted",
+                        id_key,
+                        id
+                    ),
+                    ReportCategory::UnknownPropertyValue,
+                );
+        } else {
             report.add_error(
-                format!(
-                    "The rule on the \"{}\" physical mode was not applied",
-                    physical_mode_id
-                ),
+                format!("The rule on {} \"{}\" was not applied", id_key, id),
                 ReportCategory::ObjectNotFound,
             );
         }
+        Ok(())
     }
-    collections
-        .physical_modes
-        .retain(|cm| !physical_modes_to_remove.contains(&cm.id));
-
-    collections.physical_modes.extend(new_physical_modes);
-
-    Ok(())
-}
-
-fn check_and_apply_commercial_modes_rules(
-    report: &mut Report<ReportCategory>,
-    collections: &mut Collections,
-    commercial_modes_rules: &[ObjectProperties],
-    lines_by_commercial_mode: &HashMap<String, IdxSet<Line>>,
-) -> Result<()> {
-    info!("Checking commercial modes rules.");
-    let mut commercial_modes_to_remove: HashSet<String> = HashSet::new();
-    let mut new_commercial_modes: Vec<CommercialMode> = vec![];
-
-    for pyr in commercial_modes_rules.iter() {
-        let commercial_mode_id = pyr
-            .properties
-            .get("commercial_mode_id")
-            .ok_or_else(|| format_err!("Key \"commercial_mode_id\" is required"))?
-            .as_str()
-            .ok_or_else(|| format_err!("Value for \"commercial_mode_id\" must be filled in"))?;
-
-        if !collections.commercial_modes.contains_id(commercial_mode_id) {
-            new_commercial_modes.push(serde_json::from_value(pyr.properties.clone())?)
-        }
-
-        let mut commercial_mode_rule = false;
-        for cm_grouped in &pyr.grouped_from {
-            if !collections.commercial_modes.contains_id(&cm_grouped) {
-                report.add_error(
-                    format!("The grouped commercial mode \"{}\" don't exist", cm_grouped),
-                    ReportCategory::ObjectNotFound,
-                );
-            } else if let Some(lines) = lines_by_commercial_mode.get(cm_grouped) {
-                for line_idx in lines {
-                    collections.lines.index_mut(*line_idx).commercial_mode_id =
-                        commercial_mode_id.to_string();
-                }
-                commercial_mode_rule = true;
-                commercial_modes_to_remove.insert(cm_grouped.to_string());
-            }
-        }
-        if !commercial_mode_rule {
-            report.add_error(
-                format!(
-                    "The rule on the \"{}\" commercial mode was not applied",
-                    commercial_mode_id
-                ),
-                ReportCategory::ObjectNotFound,
-            );
-        }
-    }
-    collections
-        .commercial_modes
-        .retain(|cm| !commercial_modes_to_remove.contains(&cm.id));
-
-    collections.commercial_modes.extend(new_commercial_modes);
-
-    Ok(())
-}
-
-fn check_and_apply_networks_rules(
-    report: &mut Report<ReportCategory>,
-    collections: &mut Collections,
-    networks_rules: &[ObjectProperties],
-    lines_by_network: &HashMap<String, IdxSet<Line>>,
-) -> Result<()> {
-    info!("Checking networks rules.");
-    let mut networks_to_remove: HashSet<String> = HashSet::new();
-    let mut new_networks: Vec<Network> = vec![];
-
-    for pyr in networks_rules.iter() {
-        let network_id = pyr
-            .properties
-            .get("network_id")
-            .ok_or_else(|| format_err!("Key \"network_id\" is required"))?
-            .as_str()
-            .ok_or_else(|| format_err!("Value for \"network_id\" must be filled in"))?;
-
-        if !collections.networks.contains_id(network_id) {
-            new_networks.push(serde_json::from_value(pyr.properties.clone())?)
-        }
-        let mut network_rule = false;
-        for grouped in &pyr.grouped_from {
-            if !collections.networks.contains_id(&grouped) {
-                report.add_error(
-                    format!("The grouped network \"{}\" don't exist", grouped),
-                    ReportCategory::ObjectNotFound,
-                );
-            } else if let Some(lines) = lines_by_network.get(grouped) {
-                for line_idx in lines {
-                    collections.lines.index_mut(*line_idx).network_id = network_id.to_string();
-                }
-                collections
-                    .ticket_use_perimeters
-                    .values_mut()
-                    .filter(|ticket| ticket.object_type == ModelObjectType::Network)
-                    .filter(|ticket| &ticket.object_id == grouped)
-                    .for_each(|mut ticket| ticket.object_id = network_id.to_string());
-                network_rule = true;
-                networks_to_remove.insert(grouped.to_string());
-            }
-        }
-        if !network_rule {
-            report.add_error(
-                format!("The rule on the \"{}\" network was not applied", network_id),
-                ReportCategory::ObjectNotFound,
-            );
-        }
-    }
-    collections
-        .networks
-        .retain(|cm| !networks_to_remove.contains(&cm.id));
-
-    collections.networks.extend(new_networks);
-
-    Ok(())
 }
 
 impl ObjectRule {
@@ -318,32 +275,102 @@ impl ObjectRule {
         collections: &mut Collections,
         report: &mut Report<ReportCategory>,
     ) -> Result<()> {
-        if let (Some(networks_rules), Some(lines_by_network)) =
-            (&self.configuration.networks_rules, &self.lines_by_network)
-        {
-            check_and_apply_networks_rules(report, collections, networks_rules, lines_by_network)?;
+        if let (
+            Some(networks_rules),
+            Some(lines_by_network),
+            Some(ticket_use_perimeters_by_network),
+        ) = (
+            &self.configuration.networks_rules,
+            &self.lines_by_network,
+            &self.ticket_use_perimeters_by_network,
+        ) {
+            info!("Checking networks rules.");
+            for rule in networks_rules {
+                let lines = &mut collections.lines;
+                let ticket_use_perimeters = &mut collections.ticket_use_perimeters;
+                let regroup_update = |network_id: &str, removed_id: &str| {
+                    let lines_modified =
+                        if let Some(line_indexes) = lines_by_network.get(removed_id) {
+                            for line_idx in line_indexes {
+                                lines.index_mut(*line_idx).network_id = network_id.to_string();
+                            }
+                            true
+                        } else {
+                            false
+                        };
+                    let tup_modified = if let Some(tup_indexes) =
+                        ticket_use_perimeters_by_network.get(removed_id)
+                    {
+                        for tup_idx in tup_indexes {
+                            ticket_use_perimeters.index_mut(*tup_idx).object_id =
+                                network_id.to_string();
+                        }
+                        true
+                    } else {
+                        false
+                    };
+                    lines_modified || tup_modified
+                };
+                rule.apply(
+                    "network_id",
+                    &mut collections.networks,
+                    report,
+                    regroup_update,
+                )?;
+            }
         };
         if let (Some(commercial_modes_rules), Some(lines_by_commercial_mode)) = (
             &self.configuration.commercial_modes_rules,
             &self.lines_by_commercial_mode,
         ) {
-            check_and_apply_commercial_modes_rules(
-                report,
-                collections,
-                commercial_modes_rules,
-                lines_by_commercial_mode,
-            )?;
+            info!("Checking commercial modes rules.");
+            for rule in commercial_modes_rules {
+                let lines = &mut collections.lines;
+                let regroup_update = |commercial_mode_id: &str, removed_id: &str| {
+                    if let Some(line_indexes) = lines_by_commercial_mode.get(removed_id) {
+                        for line_idx in line_indexes {
+                            lines.index_mut(*line_idx).commercial_mode_id =
+                                commercial_mode_id.to_string();
+                        }
+                        true
+                    } else {
+                        false
+                    }
+                };
+                rule.apply(
+                    "commercial_mode_id",
+                    &mut collections.commercial_modes,
+                    report,
+                    regroup_update,
+                )?;
+            }
         };
         if let (Some(physical_modes_rules), Some(vjs_by_physical_mode)) = (
             &self.configuration.physical_modes_rules,
             &self.vjs_by_physical_mode,
         ) {
-            check_and_apply_physical_modes_rules(
-                report,
-                collections,
-                physical_modes_rules,
-                vjs_by_physical_mode,
-            )?;
+            info!("Checking physical modes rules.");
+            for rule in physical_modes_rules {
+                let vehicle_journeys = &mut collections.vehicle_journeys;
+                let regroup_update = |physical_mode_id: &str, removed_id: &str| {
+                    if let Some(vehicle_journey_indexes) = vjs_by_physical_mode.get(removed_id) {
+                        for vehicle_journey_idx in vehicle_journey_indexes {
+                            vehicle_journeys
+                                .index_mut(*vehicle_journey_idx)
+                                .physical_mode_id = physical_mode_id.to_string();
+                        }
+                        true
+                    } else {
+                        false
+                    }
+                };
+                rule.apply(
+                    "physical_mode_id",
+                    &mut collections.physical_modes,
+                    report,
+                    regroup_update,
+                )?;
+            }
         };
         Ok(())
     }
